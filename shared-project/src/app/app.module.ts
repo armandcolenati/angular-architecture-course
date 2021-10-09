cd3dbddf6658e6a999530597d00ac93970844c57
Armand-Adrian Colenati <armandcolenati@gmail.com>
2021-10-09 11:00:17 +0300

    (Lab 8) Angular library

---
+++ shared-project/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedLibModule } from 'shared-lib';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    SharedLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
